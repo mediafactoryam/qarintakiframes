009804698bf3d9ed76a585fcd408658f158495b8
sandaramet <aram@gratun.am>
2021-04-09 14:21:28 +0400

    NAME and DATA_OF_DEATH front of photo

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,19 @@ $(document).ready(function () {
   for (const { name, dateOfDeath, description, link, photo } of DATA) {
     $(`
     <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-        <h2>${name}</h2>
 	    <div class="flipper">
 		    <div class="front">
-                <img src="${photo}" alt="${name}" />
-            </div>
-            <div class="back">
-                <p>${description}</p>
-                <a href="${link}" >Իմանալ Ավելին</a>
-            </div>
+          <img src="${photo}" alt="${name}" />
+          <div>
+            <h2>${name}</h2>
+            <p>${dateOfDeath}</p>
+          </div>
         </div>
-        <p>${dateOfDeath}</p>
+        <div class="back">
+          <p>${description}</p>
+          <a href="${link}" >Իմանալ Ավելին</a>
+        </div>
+      </div>
     </div>
     `).appendTo(ROOT_EL)
   }
